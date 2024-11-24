c91f0871001b18cb9026431609ae5a1cb4131ac5
Jose Jaime Come <josejaimecome@gmail.com>
2024-11-24 19:19:15 +0200

    Form to capture inputs for ML in Frontend app added

--- vuejs-frontend/src/components/FormResult.vue
+++ vuejs-frontend/src/components/FormResult.vue
@@ -1,3 +1,33 @@
 <template>
   <h1>Home Page</h1>
+  <div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1">Full Name</span>
+  <input type="text" class="form-control" placeholder="Full Name" aria-label="Fullname" aria-describedby="basic-addon1">
+</div>
+<div>
+  <span style="font-size: 12px; color: blue;">Fill with numbers that range from 0.1 to 9.9</span>
+</div>
+<table>
+  <tr>
+    <td></td><td><strong>Length</strong></td><td><strong>Width</strong></td>
+  </tr>
+  <tr>
+    <td><strong>Sepal</strong></td>
+    <td>
+      <input type="text" class="form-control" aria-describedby="basic-addon1">
+    </td>
+    <td>
+      <input type="text" class="form-control" aria-describedby="basic-addon1">
+    </td>
+  </tr>
+  <tr>
+    <td><strong>Petal</strong></td>
+    <td>
+      <input type="text" class="form-control" aria-describedby="basic-addon1">
+    </td>
+    <td>
+      <input type="text" class="form-control" aria-describedby="basic-addon1">
+    </td>
+  </tr>
+</table>
 </template>
